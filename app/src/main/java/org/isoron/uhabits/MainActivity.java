12c9de22a73139c23523f71cd983e9b0d6e1ec52
Alinson Xavier <axavier@uwaterloo.ca>
2016-02-18 14:45:48 -0500

    Update copyright

--- app/src/main/java/org/isoron/uhabits/MainActivity.java
+++ app/src/main/java/org/isoron/uhabits/MainActivity.java
@@ -1,16 +1,29 @@
+/* Copyright (C) 2016 Alinson Santos Xavier
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied  warranty of MERCHANTABILITY or
+ * FITNESS  FOR  A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You  should  have  received  a  copy  of the GNU General Public License
+ * along  with  this  program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.isoron.uhabits;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import org.isoron.helpers.Command;
 import org.isoron.helpers.ReplayableActivity;
 import org.isoron.uhabits.dialogs.ListHabitsFragment;
 import org.isoron.uhabits.models.Habit;
