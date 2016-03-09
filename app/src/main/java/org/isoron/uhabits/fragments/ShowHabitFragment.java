d038bdb74111ac642075dc48d5faeb0335bddb36
Alinson Xavier <axavier@uwaterloo.ca>
2016-03-08 21:50:10 -0500

    Update widgets after history editor closes

--- app/src/main/java/org/isoron/uhabits/fragments/ShowHabitFragment.java
+++ app/src/main/java/org/isoron/uhabits/fragments/ShowHabitFragment.java
@@ -34,6 +34,7 @@ import android.widget.TextView;
 import org.isoron.helpers.ColorHelper;
 import org.isoron.helpers.Command;
 import org.isoron.helpers.DialogHelper;
+import org.isoron.uhabits.MainActivity;
 import org.isoron.uhabits.R;
 import org.isoron.uhabits.ShowHabitActivity;
 import org.isoron.uhabits.dialogs.HistoryEditorDialog;
@@ -173,6 +174,7 @@ public class ShowHabitFragment extends Fragment
     public void onHistoryEditorClosed()
     {
         refreshData();
+        MainActivity.updateWidgets(getActivity());
     }
 
     private void refreshData()
