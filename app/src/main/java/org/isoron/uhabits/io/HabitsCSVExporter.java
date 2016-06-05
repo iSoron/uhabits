cabcd5b1bf65d601b3aab18f081f560cfe60629f
Alinson Xavier <axavier@uwaterloo.ca>
2016-06-04 22:06:01 -0400

    CSV export: allow spaces on filename and fix tests

--- app/src/main/java/org/isoron/uhabits/io/HabitsCSVExporter.java
+++ app/src/main/java/org/isoron/uhabits/io/HabitsCSVExporter.java
@@ -81,7 +81,7 @@ public class HabitsCSVExporter
     @NonNull
     private String sanitizeFilename(String name)
     {
-        String s = name.replaceAll("[^a-zA-Z0-9\\._-]+", "");
+        String s = name.replaceAll("[^ a-zA-Z0-9\\._-]+", "");
         return s.substring(0, Math.min(s.length(), 100));
     }
 
