e052a144bd4b7bf3b9b50658910a0561c0507f31
Luboš Luňák <l.lunak@centrum.cz>
2017-09-18 23:13:53 +0200

    fix testTruncate_dayOfWeek failing with some locales

--- uhabits-core/src/main/java/org/isoron/uhabits/core/utils/DateUtils.java
+++ uhabits-core/src/main/java/org/isoron/uhabits/core/utils/DateUtils.java
@@ -69,7 +69,7 @@ public abstract class DateUtils
     private static GregorianCalendar getCalendar(long timestamp)
     {
         GregorianCalendar day =
-            new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+            new GregorianCalendar(TimeZone.getTimeZone("GMT"), getLocale());
         day.setTimeInMillis(timestamp);
         return day;
     }
