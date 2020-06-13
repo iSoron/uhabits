c8e3735dd6ba0db56d406c7e1a93c7f974e14305
Andrea Gelmini <andrea.gelmini@gelma.net>
2020-06-13 18:25:05 +0200

    Fix typos

--- android/tools/coverage-badge/badge.py
+++ android/tools/coverage-badge/badge.py
@@ -55,7 +55,7 @@ def get_total(report):
 
 def get_color(total):
     """
-    Return color for current coverage precent
+    Return color for current coverage percent
     """
     try:
         xtotal = int(total)
