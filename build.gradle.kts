a58a8005e11add036184264f7ba7fb73b135621a
Quentin Hibon <qh.public@yahoo.com>
2021-09-02 20:49:11 +0200

    Bump AGP from 4.2.2 to 7.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val kotlinVersion = "1.5.0"
-    id("com.android.application") version ("4.2.2") apply (false)
+    id("com.android.application") version ("7.0.2") apply (false)
     id("org.jetbrains.kotlin.android") version kotlinVersion apply (false)
     id("org.jetbrains.kotlin.kapt") version kotlinVersion apply (false)
     id("org.jetbrains.kotlin.android.extensions") version kotlinVersion apply (false)
