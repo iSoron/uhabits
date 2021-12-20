2d820495001f9a082b17cb8779bb4792f56e4641
sgallese <sebastiangallese@gmail.com>
2021-12-19 20:53:15 -0800

    Update tests post merge

--- uhabits-core/src/jvmTest/java/org/isoron/uhabits/core/ui/views/HistoryChartTest.kt
+++ uhabits-core/src/jvmTest/java/org/isoron/uhabits/core/ui/views/HistoryChartTest.kt
@@ -122,20 +122,20 @@ class HistoryChartTest {
 
         // Click top left date
         view.onLongClick(20.0, 46.0)
-        verify(dateClickedListener).onDateLongPress(LocalDate(2014, 10, 26))
+        verify(dateClickedListener).onDateLongPress(LocalDate(2014, Month.OCTOBER, 26))
         reset(dateClickedListener)
         view.onLongClick(2.0, 28.0)
-        verify(dateClickedListener).onDateLongPress(LocalDate(2014, 10, 26))
+        verify(dateClickedListener).onDateLongPress(LocalDate(2014, Month.OCTOBER, 26))
         reset(dateClickedListener)
 
         // Click date in the middle
         view.onLongClick(163.0, 113.0)
-        verify(dateClickedListener).onDateLongPress(LocalDate(2014, 12, 10))
+        verify(dateClickedListener).onDateLongPress(LocalDate(2014, Month.DECEMBER, 10))
         reset(dateClickedListener)
 
         // Click today
         view.onLongClick(336.0, 37.0)
-        verify(dateClickedListener).onDateLongPress(LocalDate(2015, 1, 25))
+        verify(dateClickedListener).onDateLongPress(LocalDate(2015, Month.JANUARY, 25))
         reset(dateClickedListener)
 
         // Click header
