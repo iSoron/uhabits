d47878d6886ad81a6277d31d57322853dcf6549c
Edgiest05 <mattiagerosa05@gmail.com>
2022-10-13 21:09:50 +0200

    Fixed Italian decimals issue
    This commit fixes iSoron/uhabits#1532
    Comma replaced with dot when reading from dialog input

--- uhabits-android/src/main/java/org/isoron/uhabits/activities/common/dialogs/NumberPopup.kt
+++ uhabits-android/src/main/java/org/isoron/uhabits/activities/common/dialogs/NumberPopup.kt
@@ -108,7 +108,8 @@ class NumberPopup(
     }
 
     fun save() {
-        val value = view.value.text.toString().toDoubleOrNull() ?: originalValue
+        val tmp = view.value.text.toString().replace(',','.')
+        val value = tmp.toDoubleOrNull() ?: originalValue
         val notes = view.notes.text.toString()
         onToggle(value, notes)
         dialog.dismiss()
