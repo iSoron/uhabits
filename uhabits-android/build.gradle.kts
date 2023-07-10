fe41bdb8691630538c96530659bc017ed6fc94b2
Quentin Hibon <qh.public@yahoo.com>
2023-07-10 20:04:32 +0200

    Bump minSdk back to 28 from 23
    Fixes #1758.

--- uhabits-android/build.gradle.kts
+++ uhabits-android/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     defaultConfig {
         versionCode = 20200
         versionName = "2.2.0"
-        minSdk = 23
+        minSdk = 28
         targetSdk = 31
         applicationId = "org.isoron.uhabits"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
