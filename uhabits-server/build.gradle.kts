7e7f68282bd9f5f6fed4f699247b95a060431f48
Alinson S. Xavier <git@axavier.org>
2023-11-29 19:38:21 -0600

    Update Kotlin

--- uhabits-server/build.gradle.kts
+++ uhabits-server/build.gradle.kts
@@ -38,7 +38,7 @@ application {
 
 dependencies {
     val ktorVersion = "1.6.8"
-    val kotlinVersion = "1.8.20"
+    val kotlinVersion = "1.9.21"
     val logbackVersion = "1.4.9"
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
