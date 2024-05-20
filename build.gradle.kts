732ec1c70aad2dd7da066652e127dd6d510c9e2d
Alinson S. Xavier <git@axavier.org>
2024-05-19 20:36:43 -0500

    Update AGP; disable lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val kotlinVersion = "1.9.22"
-    id("com.android.application") version "8.1.4" apply (false)
+    id("com.android.application") version "8.4.0" apply (false)
     id("org.jetbrains.kotlin.android") version kotlinVersion apply (false)
     id("org.jetbrains.kotlin.kapt") version kotlinVersion apply (false)
     id("org.jetbrains.kotlin.multiplatform") version kotlinVersion apply (false)
