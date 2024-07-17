cf045a4cd51cfd6732a3b950fa34d46fe88fa413
Chris Smowton <smowton@github.com>
2024-07-17 15:40:36 +0100

    Add repository now required for old kotlin-css-jvm versions
    Gradle Plugin Portal was previously making kotlin-css-jvm 1.0.0-pre.148-kotlin-1.4.30 available by virtue of mirroring JCenter. Now that Gradle have stopped doing that in preparation for JCenter to go away, we need to retrieve the old version from a different repository.
    
    If this is bumped forwards to Kotlin 1.5.0+ it will become possible to get the package from Central instead.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,5 +18,6 @@ allprojects {
         maven(url = "https://plugins.gradle.org/m2/")
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
         maven(url = "https://jitpack.io")
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers/") // Repository for kotlin-css-jvm old versions, now that the Gradle Plugin Portal no longer brings these in by mirroring JCenter
     }
 }
